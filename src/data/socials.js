9a932b1642c0889ac234dbddb92f200ddf42863c
Santu-developer <santoshpatidar.dev@gmail.com>
2026-04-27 00:05:31 +0530

    update mobile number

--- src/data/socials.js
+++ src/data/socials.js
@@ -5,7 +5,7 @@ const socials = [
   { name: 'GitHub', url: 'https://github.com/Santu-developer', icon: FiGithub },
   { name: 'LinkedIn', url: 'https://linkedin.com/in/santosh-patidar', icon: FiLinkedin },
   { name: 'Email', url: 'santoshpatidar.dev@gmail.com', icon: FiMail },
-  { name: 'Phone', url: 'tel:+919981689594', icon: FiPhone },
+  { name: 'Phone', url: 'tel:+919404387846', icon: FiPhone },
   // Optional: keep or remove Twitter; replace icon if desired
 ]
 
